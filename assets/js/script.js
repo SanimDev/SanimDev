c3c78056431f2303fc9f268345b8c26311133b19
Ly Sanim <saniml@cats.com.kh>
2026-03-27 16:43:09 +0700

    add url for cat websit & remove url of computer shop to empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,17 +191,52 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
+// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+// (function () {
+//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+//     s1.async = true;
+//     s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//     s1.charset = 'UTF-8';
+//     s1.setAttribute('crossorigin', '*');
+//     s0.parentNode.insertBefore(s1, s0);
+// })();
 // End of Tawk.to Live Chat
 
+var Tawk_API = Tawk_API || {};
+Tawk_API.onLoad = function(){
+  Tawk_API.endChat(); // clears current chat session
+};
+
+var Tawk_API = Tawk_API || {};
+Tawk_API.customStyle = {
+  visibility: {
+    desktop: {
+      position: 'bl',
+      xOffset: 20,
+      yOffset: 65
+    },
+    mobile: {
+      position: 'bl',
+      xOffset: 10,
+      yOffset: 25
+    }
+  }
+};
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69c625711b017d1c37d0e917/1jkn04guf';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
+
+ 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
